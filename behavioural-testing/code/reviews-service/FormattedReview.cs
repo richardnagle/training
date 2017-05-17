64ae1ce81a2e414cb1549244714bec82a599454f
Richard Nagle <richard.nagle@firsthelpline.com>
2017-05-17 11:58:18 +0100

    fixed all html formatting in a naive way

--- behavioural-testing/code/reviews-service/FormattedReview.cs
+++ behavioural-testing/code/reviews-service/FormattedReview.cs
@@ -17,13 +17,20 @@ namespace reviews_service
         {
             long isbn;
 
-            var sections = postedReview.Sections?.ToDictionary(
-                key => key.Name,
-                val => val.Text);
+            var htmlText = string.Empty;
 
-            var htmlText = sections != null
-                ? $"<h1>{sections["Title"]}</h1><h2>{sections["SubTitle"]}</h2><p>{sections["Body"]}</p>"
-                : string.Empty;
+            if (postedReview.Sections != null)
+            {
+                var sections = postedReview.Sections.ToDictionary(
+                    key => key.Name,
+                    val => val.Text);
+
+                var title = sections.ContainsKey("Title") ? $"<h1>{sections["Title"]}</h1>" : string.Empty;
+                var subTitle = sections.ContainsKey("SubTitle") ? $"<h2>{sections["SubTitle"]}</h2>" : string.Empty;
+                var body = sections.ContainsKey("Body") ? $"<p>{sections["Body"]}</p>" : string.Empty;
+
+                htmlText = string.Concat(title, subTitle, body);
+            }
 
             return new FormattedReview(
                 long.TryParse(postedReview.ISBN, out isbn) ? isbn : 0,
