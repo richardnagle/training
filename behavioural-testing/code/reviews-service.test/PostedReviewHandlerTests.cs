50df60d48fd340d335bfcb0c0aa0c9c74425306a
Richard Nagle <richard.nagle@firsthelpline.com>
2017-06-07 15:24:44 +0100

    integrate validatation and saving into Handler

---
+++ behavioural-testing/code/reviews-service.test/PostedReviewHandlerTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using reviews_service.infrastructure;
+
+namespace reviews_service.test
+{
+    [TestFixture]
+    public class PostedReviewHandlerTests
+    {
+        [Test]
+        public void Responds_with_http_415_when_content_type_header_invalid()
+        {
+            var request = new Request<PostedReview>(new PostedReview());
+
+            var validator = Substitute.For<IReviewValidator>();
+            validator.ValidateContentType(request).Returns(false);
+            validator.ValidateReferer(request).Returns(true);
+            validator.ValidateISBN(request).Returns(true);
+
+            var handler = new PostedReviewHandler(Substitute.For<ISaveReviews>(), validator);
+            var response = handler.Handle(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(415));
+            Assert.That(response.Error, Is.EqualTo("Incorrect content type"));
+        }
+
+        [Test]
+        public void Responds_with_http_400_when_referer_uri_is_invalid()
+        {
+            var request = new Request<PostedReview>(new PostedReview());
+
+            var validator = Substitute.For<IReviewValidator>();
+            validator.ValidateContentType(request).Returns(true);
+            validator.ValidateReferer(request).Returns(false);
+            validator.ValidateISBN(request).Returns(true);
+
+            var handler = new PostedReviewHandler(Substitute.For<ISaveReviews>(), validator);
+            var response = handler.Handle(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(400));
+            Assert.That(response.Error, Is.EqualTo("Bad referer uri"));
+        }
+
+        [Test]
+        public void Responds_with_http_400_when_isbn_is_invalid()
+        {
+            var request = new Request<PostedReview>(new PostedReview());
+
+            var validator = Substitute.For<IReviewValidator>();
+            validator.ValidateContentType(request).Returns(true);
+            validator.ValidateReferer(request).Returns(true);
+            validator.ValidateISBN(request).Returns(false);
+
+            var handler = new PostedReviewHandler(Substitute.For<ISaveReviews>(), validator);
+            var response = handler.Handle(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(400));
+            Assert.That(response.Error, Is.EqualTo("Invalid ISBN"));
+        }
+
+        [Test]
+        public void Responds_with_http_201_when_request_is_valid()
+        {
+            var request = new Request<PostedReview>(new PostedReview());
+
+            var validator = Substitute.For<IReviewValidator>();
+            validator.ValidateContentType(request).Returns(true);
+            validator.ValidateReferer(request).Returns(true);
+            validator.ValidateISBN(request).Returns(true);
+
+            var handler = new PostedReviewHandler(Substitute.For<ISaveReviews>(), validator);
+            var response = handler.Handle(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(201));
+            Assert.That(response.Error, Is.Empty);
+        }
+
+        [Test]
+        public void Saves_review_when_request_is_valid()
+        {
+            var request = new Request<PostedReview>(new PostedReview());
+
+            var validator = Substitute.For<IReviewValidator>();
+            validator.ValidateContentType(request).Returns(true);
+            validator.ValidateReferer(request).Returns(true);
+            validator.ValidateISBN(request).Returns(true);
+
+            var databaseService = Substitute.For<ISaveReviews>();
+
+            var handler = new PostedReviewHandler(databaseService, validator);
+            handler.Handle(request);
+
+            databaseService.Received().Insert(Arg.Any<ReviewDto>());
+        }
+    }
+}
---
+++ behavioural-testing/code/reviews-service/PostedReviewHandler.cs
@@ -0,0 +1,38 @@
+﻿using reviews_service.infrastructure;
+
+namespace reviews_service
+{
+    public class PostedReviewHandler: IHandle<PostedReview>
+    {
+        private readonly ISaveReviews _databaseService;
+        private readonly IReviewValidator _validator;
+
+        public PostedReviewHandler(ISaveReviews databaseService, IReviewValidator validator)
+        {
+            _databaseService = databaseService;
+            _validator = validator;
+        }
+
+        public Response Handle(Request<PostedReview> request)
+        {
+            if (!_validator.ValidateContentType(request))
+            {
+                return new Response(415, "Incorrect content type");
+            }
+
+            if (!_validator.ValidateReferer(request))
+            {
+                return new Response(400, "Bad referer uri");
+            }
+
+            if (!_validator.ValidateISBN(request))
+            {
+                return new Response(400, "Invalid ISBN");
+            }
+
+            _databaseService.Insert(new ReviewDto());
+
+            return new Response(201, "");
+        }
+    }
+}
