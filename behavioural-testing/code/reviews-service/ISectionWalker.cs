898b2c926030897b5686bcbad4c1cdab91031fa7
Richard Nagle <richard.nagle@firsthelpline.com>
2017-06-07 14:30:15 +0100

    Create SectionWalker component

---
+++ behavioural-testing/code/reviews-service/ISectionWalker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using reviews_service.infrastructure;
+
+namespace reviews_service
+{
+    public interface ISectionWalker
+    {
+        string GetText(IEnumerable<ReviewSection> sections, string sectionName);
+    }
+}
