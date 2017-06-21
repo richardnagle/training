7ec30def8cfc487709e87fe9d847c3e248d6cb0d
Richard Nagle <richard.nagle@firsthelpline.com>
2017-06-21 12:53:55 +0100

    add test for saving to the database

--- behavioural-testing/code/reviews-service.test/ReviewHandlerTests.cs
+++ behavioural-testing/code/reviews-service.test/ReviewHandlerTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NSubstitute;
+using NUnit.Framework;
 using reviews_service.infrastructure;
 
 namespace reviews_service.test
@@ -9,13 +12,55 @@ namespace reviews_service.test
         [Test]
         public void Valid_review_returns_http_201_with_no_error_message()
         {
-            var request = new Request<PostedReview>(new PostedReview());
+            var handler = new ReviewHandler(new ReviewDtoMapper(), new SectionWalker(), new ReviewHtmlFormatter(), Substitute.For<ISaveReviews>());
 
-            var handler = new ReviewHandler();
+            var request = new Request<PostedReview>(
+                new PostedReview
+                {
+                    ISBN = "1234567890123",
+                    Sections = new ReviewSection[0],
+                },
+                new Dictionary<string, string>
+                {
+                    ["Referer"] = "http://the-referer.com"
+                });
+
             var response = handler.Handle(request);
 
             Assert.That(response.StatusCode, Is.EqualTo(201));
             Assert.That(response.Error, Is.Empty);
+        }
+
+        [Test]
+        public void Valid_review_is_saved_in_database()
+        {
+            var database = Substitute.For<ISaveReviews>();
+
+            var handler = new ReviewHandler(new ReviewDtoMapper(), new SectionWalker(), new ReviewHtmlFormatter(), database);
+            var request = new Request<PostedReview>(
+                new PostedReview
+                {
+                    ISBN = "1234567890123",
+                    Sections = new[]
+                    {
+                        new ReviewSection { Name = "title", Text = "the-title"},
+                        new ReviewSection { Name = "subtitle", Text = "the-sub-title"},
+                        new ReviewSection { Name = "body", Text = "the-body"},
+                    }
+                },
+                new Dictionary<string, string>
+                {
+                    ["Referer"] = "http://the-referer.com"
+                });
+
+            var response = handler.Handle(request);
+
+            database
+                .Received()
+                .Insert(Arg.Is<ReviewDto>(dto =>
+                        dto.ISBN == 1234567890123 &&
+                        Regex.IsMatch(dto.Text, "(?s).*the-title.*the-sub-title.*the-body.*") &&
+                        dto.Uri == "http://the-referer.com"));
         }
     }
 }
--- behavioural-testing/code/reviews-service/ReviewHandler.cs
+++ behavioural-testing/code/reviews-service/ReviewHandler.cs
@@ -1,11 +1,37 @@
-﻿using reviews_service.infrastructure;
+﻿using System;
+using reviews_service.infrastructure;
 
 namespace reviews_service
 {
     public class ReviewHandler: IHandle<PostedReview>
     {
+        private readonly ReviewDtoMapper _dtoMapper;
+        private readonly SectionWalker _sectionWalker;
+        private readonly ReviewHtmlFormatter _htmlFormatter;
+        private readonly ISaveReviews _database;
+
+        public ReviewHandler(ReviewDtoMapper dtoMapper, SectionWalker sectionWalker, ReviewHtmlFormatter htmlFormatter, ISaveReviews database)
+        {
+            _dtoMapper = dtoMapper;
+            _sectionWalker = sectionWalker;
+            _htmlFormatter = htmlFormatter;
+            _database = database;
+        }
+
         public Response Handle(Request<PostedReview> request)
         {
+            var title = _sectionWalker.GetText(request.Body.Sections, "title");
+            var subTitle = _sectionWalker.GetText(request.Body.Sections, "subtitle");
+            var body = _sectionWalker.GetText(request.Body.Sections, "body");
+            var bodyHtml = _htmlFormatter.Format(title, subTitle, body);
+
+            var dto = new ReviewDto();
+            _dtoMapper.MapBodyFields(request.Body, dto);
+            _dtoMapper.MapText(bodyHtml, dto);
+            _dtoMapper.MapHttpHeaders(request.Headers, dto);
+
+            _database.Insert(dto);
+
             return new Response(201,"");
         }
     }
