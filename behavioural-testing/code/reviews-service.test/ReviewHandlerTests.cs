f03c103804c690da0cef94ff9098e1f5dca30427
Richard Nagle <richard.nagle@firsthelpline.com>
2017-06-21 11:41:10 +0100

    Add first success unit test for ReviewHandler

---
+++ behavioural-testing/code/reviews-service.test/ReviewHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using reviews_service.infrastructure;
+
+namespace reviews_service.test
+{
+    [TestFixture]
+    public class ReviewHandlerTests
+    {
+        [Test]
+        public void Valid_review_returns_http_201_with_no_error_message()
+        {
+            var request = new Request<PostedReview>(new PostedReview());
+
+            var handler = new ReviewHandler();
+            var response = handler.Handle(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(201));
+            Assert.That(response.Error, Is.Empty);
+        }
+    }
+}
---
+++ behavioural-testing/code/reviews-service/ReviewHandler.cs
@@ -0,0 +1,12 @@
+﻿using reviews_service.infrastructure;
+
+namespace reviews_service
+{
+    public class ReviewHandler: IHandle<PostedReview>
+    {
+        public Response Handle(Request<PostedReview> request)
+        {
+            return new Response(201,"");
+        }
+    }
+}
