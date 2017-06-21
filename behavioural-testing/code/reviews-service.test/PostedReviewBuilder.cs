aff7d3ab577fc1a96818043f6c09f514d062a313
Richard Nagle <richard.nagle@firsthelpline.com>
2017-06-21 17:13:17 +0100

    create PostedReviewBuilder

---
+++ behavioural-testing/code/reviews-service.test/PostedReviewBuilder.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using reviews_service.infrastructure;
+
+namespace reviews_service.test
+{
+    public class PostedReviewBuilder
+    {
+        private string _isbn = "1234567890123";
+        private string _bodyTitle = "the-title";
+        private string _bodySubTitle = "the-sub-title";
+        private string _bodyText = "the-body";
+        private string _refererUrl = "http://the-referer.com";
+
+        public PostedReviewBuilder WithIsbn(string isbn)
+        {
+            _isbn = isbn;
+            return this;
+        }
+
+        public PostedReviewBuilder WithBodyTitle(string bodyTitle)
+        {
+            _bodyTitle = bodyTitle;
+            return this;
+        }
+
+        public PostedReviewBuilder WithBodySubTitle(string bodySubTitle)
+        {
+            _bodySubTitle = bodySubTitle;
+            return this;
+        }
+
+        public PostedReviewBuilder WithBodyText(string bodyText)
+        {
+            _bodyText = bodyText;
+            return this;
+        }
+
+        public PostedReviewBuilder WithRefererUrl(string refererUrl)
+        {
+            _refererUrl = refererUrl;
+            return this;
+        }
+
+        public Request<PostedReview> Build()
+        {
+            return new Request<PostedReview>(
+                new PostedReview
+                {
+                    ISBN = _isbn,
+                    Sections = new[]
+                    {
+                        new ReviewSection {Name = "title", Text = _bodyTitle},
+                        new ReviewSection {Name = "subtitle", Text = _bodySubTitle},
+                        new ReviewSection {Name = "body", Text = _bodyText},
+                    }
+                },
+                new Dictionary<string, string>()
+                {
+                    ["Referer"] = _refererUrl
+                });
+        }
+    }
+}
--- behavioural-testing/code/reviews-service.test/ReviewHandlerTests.cs
+++ behavioural-testing/code/reviews-service.test/ReviewHandlerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using NSubstitute;
 using NUnit.Framework;
 using reviews_service.infrastructure;
@@ -13,17 +12,7 @@ namespace reviews_service.test
         public void Valid_review_returns_http_201_with_no_error_message()
         {
             var handler = new ReviewHandler(new ReviewDtoMapper(), new SectionWalker(), new ReviewHtmlFormatter(), Substitute.For<ISaveReviews>());
-
-            var request = new Request<PostedReview>(
-                new PostedReview
-                {
-                    ISBN = "1234567890123",
-                    Sections = new ReviewSection[0],
-                },
-                new Dictionary<string, string>
-                {
-                    ["Referer"] = "http://the-referer.com"
-                });
+            var request = new PostedReviewBuilder().Build();
 
             var response = handler.Handle(request);
 
@@ -37,21 +26,13 @@ namespace reviews_service.test
             var database = Substitute.For<ISaveReviews>();
 
             var handler = new ReviewHandler(new ReviewDtoMapper(), new SectionWalker(), new ReviewHtmlFormatter(), database);
-            var request = new Request<PostedReview>(
-                new PostedReview
-                {
-                    ISBN = "1234567890123",
-                    Sections = new[]
-                    {
-                        new ReviewSection { Name = "title", Text = "the-title"},
-                        new ReviewSection { Name = "subtitle", Text = "the-sub-title"},
-                        new ReviewSection { Name = "body", Text = "the-body"},
-                    }
-                },
-                new Dictionary<string, string>
-                {
-                    ["Referer"] = "http://the-referer.com"
-                });
+            var request = new PostedReviewBuilder()
+                .WithIsbn("1234567890123")
+                .WithBodyTitle("the-title")
+                .WithBodySubTitle("the-sub-title")
+                .WithBodyText("the-body")
+                .WithRefererUrl("http://the-referer.com")
+                .Build();
 
             var response = handler.Handle(request);
 
