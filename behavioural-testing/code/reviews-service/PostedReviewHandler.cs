844f7c87cabb0eafa43b4c5b842462ec71cc24c0
Richard Nagle <richard.nagle@firsthelpline.com>
2017-07-19 13:16:49 +0100

    refactor - extract review class

--- behavioural-testing/code/reviews-service/PostedReviewHandler.cs
+++ behavioural-testing/code/reviews-service/PostedReviewHandler.cs
@@ -13,32 +13,14 @@ namespace reviews_service
 
         public Response Handle(Request<PostedReview> request)
         {
-            var postedReview = request.Body;
-            var headers = new HttpHeaders(request.Headers);
-            var isbn = new Isbn(postedReview.ISBN);
-            var htmlBody = new HtmlBody(postedReview.Sections);
-            var reviewer = new Reviewer(postedReview.Reviewer);
+            var review = new Review(request.Body, new HttpHeaders(request.Headers));
 
-            var validators = new IValidateAReview[] {headers.ContentType, headers.Referer, isbn};
-
-            foreach (var validator in validators)
+            if (review.IsInvalid())
             {
-                if (validator.IsInvalid())
-                {
-                    return validator.GetErrorResponse();
-                }
+                return review.GetErrorResponse();
             }
 
-            var populators = new IPopulateReviewDto[] {isbn, headers.Referer, htmlBody, reviewer};
-
-            var reviewDto = new ReviewDto();
-
-            foreach (var populator in populators)
-            {
-                populator.Populate(reviewDto);
-            }
-
-            _database.Insert(reviewDto);
+            _database.Insert(review.ToReviewDto());
 
             return new Response(201, string.Empty);
         }
---
+++ behavioural-testing/code/reviews-service/Review.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using reviews_service.infrastructure;
+
+namespace reviews_service
+{
+    public class Review
+    {
+        private readonly ContentType _contentType;
+        private readonly Referer _referer;
+        private readonly Isbn _isbn;
+        private readonly HtmlBody _body;
+        private readonly Reviewer _reviewer;
+
+        public Review(PostedReview postedReview, HttpHeaders headers)
+        {
+            _contentType = headers.ContentType;
+            _referer = headers.Referer;
+            _isbn = new Isbn(postedReview.ISBN);
+            _body = new HtmlBody(postedReview.Sections);
+            _reviewer = new Reviewer(postedReview.Reviewer);
+        }
+
+        public bool IsInvalid()
+        {
+            return GetValidators().Any(x => x.IsInvalid());
+        }
+
+        public Response GetErrorResponse()
+        {
+            return GetValidators().FirstOrDefault(x => x.IsInvalid())?.GetErrorResponse();
+        }
+
+        private IEnumerable<IValidateAReview> GetValidators()
+        {
+            yield return _contentType;
+            yield return _referer;
+            yield return _isbn;
+        }
+
+        public ReviewDto ToReviewDto()
+        {
+            var reviewDto = new ReviewDto();
+
+            foreach (var populator in GetReviewDtoPopulators())
+            {
+                populator.Populate(reviewDto);
+            }
+
+            return reviewDto;
+        }
+
+        private IEnumerable<IPopulateReviewDto> GetReviewDtoPopulators()
+        {
+            yield return _isbn;
+            yield return _referer;
+            yield return _body;
+            yield return _reviewer;
+        }
+    }
+}
